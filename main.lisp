d83592841efa7e89eb97beca338ab11900fc6629
Joshua Gallagher <joshgallagher24@gmail.com>
2019-01-01 05:43:06 +0000

    Created 'make-perceptron' function - returns a weighted list

--- main.lisp
+++ main.lisp
@@ -1,3 +1,10 @@
 (defun l () 
     (load "main.lisp"))
 
+(setf *house-factors 
+    '(cheap near-shops near-uni))
+
+(defun make-perceptron (l)
+    (let ((weighted-list nil))
+        (dolist (element l (reverse (push (list 'bias 1) weighted-list)))
+            (push (list element 1) weighted-list))))
