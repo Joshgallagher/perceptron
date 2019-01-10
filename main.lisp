32b0550ec67fec24f34fbb14658e20532df9b15b
Joshua Gallagher <joshgallagher24@gmail.com>
2019-01-10 16:29:15 +0000

    Created 'make-house' function. Changed 'make-perceptron' function parameter name

--- main.lisp
+++ main.lisp
@@ -4,7 +4,12 @@
 (setf *house-factors 
     '(cheap near-shops near-uni))
 
-(defun make-perceptron (l)
+(defun make-perceptron (factors-list)
     (let ((weighted-list nil))
-        (dolist (element l (reverse (push (list 'bias 1) weighted-list)))
+        (dolist (element factors-list (reverse (push (list 'bias 1) weighted-list)))
             (push (list element 1) weighted-list))))
+
+(defun make-house (factors-list weights-list)
+    (let ((house nil))
+        (dolist (element factors-list (reverse house))
+            (setf house (push (list element (first weights-list)) house) weights-list (rest weights-list)))))
